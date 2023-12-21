c0b77c3857e4db55cfe2556afc371e9f1b24770d
Arpan Jain <arpanjain231995@gmail.com>
2023-12-21 14:16:39 +0530

    ES6 Counter Class Assignment Complete 21-12

--- JS_ES6_I/COUNTER_CLASS/index.js
+++ JS_ES6_I/COUNTER_CLASS/index.js
@@ -1,6 +1,57 @@
-class counter{
+class Counter{
     constructor(){
-        this.countValue = 0;
-        this.autoIncrement = false;
+        this.toggleBtn = document.getElementById('startCount')
+        this.incrementBtn = document.getElementById('incrementCount')
+        this.decrementBtn = document.getElementById('decrementCount')
+        this.counterValue = document.getElementById('counterDisplay')
+        this.value = 0
+        this.intervalID = null
+        this.autoIncrementEnabled = false
+
+        this.toggleBtn.addEventListener('click', this.toggle.bind(this));
+        this.incrementBtn.addEventListener('click', this.increment.bind(this));
+        this.decrementBtn.addEventListener('click', this.decrement.bind(this));
+    }
+
+    toggle(){
+        if (this.autoIncrementEnabled) {
+            this.stopAutoIncrement()
+        }
+        else {
+            this.startAutoIncrement()
+        }
+    }
+
+    startAutoIncrement(){
+        this.autoIncrementEnabled = true;
+        this.intervalId = setInterval(() => {
+            this.value++
+            this.updateCounter()
+        }, 1000)
+        this.toggleBtn.textContent = 'Stop'
+    }
+
+    stopAutoIncrement(){
+        this.autoIncrementEnabled = false
+        clearInterval(this.intervalId)
+        this.toggleBtn.textContent = 'Start'
+    }
+
+    increment(){
+        this.stopAutoIncrement()
+        this.value++
+        this.updateCounter()
+    }
+
+    decrement(){
+        this.stopAutoIncrement()
+        this.value--
+        this.updateCounter()
+    }
+
+    updateCounter(){
+        this.counterValue.textContent = this.value
     }
 }
+
+const counterInstance = new Counter()
